46e884419eb24dbca2001afe8a24fdbe62591be4
Yomi <abyomi0@gmail.com>
2016-05-21 02:25:09 -0400

    Upload finished exercise.

--- chapter3/kiloconverter.py
+++ chapter3/kiloconverter.py
@@ -1,16 +1,14 @@
-# Design a modular program that asks the user to enter a distance in kilometers, and then converts that distance to miles. The conversion formula is as follows: Miles = kilometers x 0.6214
- 
+# Design a modular program that asks the user to enter a distance in kilometers, and then converts that distance to miles. The conversion formula is as follows: miles = kilometers x 0.6214
 
-def userinput():
-    kilometers = int(input("Please enter a distance in kilometers: "))
-    return kilometers
+def main():
+    kilometers = float(input("Please enter a distance in kilometers to be converted to miles: "))
+    convert_to_kilo(kilometers)
     
-def convert(kilometers):
-    conv_to_miles = kilometers * 0.6214
+def convert_to_kilo(user_kilometers):
+    """
+    convert_to_kilo accepts exaclty one value (user_kilometers), converts it to miles and then prints the result.
+    """
+    miles = (user_kilometers * 0.6214)
+    print(miles, "miles")
     
-def result(kilometers):
-    print(kilometers + "is" + miles)
-    
-userinput()
-convert()
-result()
+main()
